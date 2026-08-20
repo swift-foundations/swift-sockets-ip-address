19c548aa49eaeffa4dec1320d8fb847c912880c3
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:03:07 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-sockets-ip-address",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
-        .library(name: "Sockets IP Address", targets: ["Sockets IP Address"]),
+        .library(name: "Sockets IP Address", targets: ["Sockets IP Address"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-foundations/swift-sockets.git", branch: "main"),
